cb5a46b7f6cadef2388f97d29e287513a35dcaec
sbsssb <been4022@gmail.com>
2025-06-10 15:33:12 +0900

    테스트

---
+++ lib/test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutterteam4/upload_region.dart';
+
+class UploadTestPage extends StatelessWidget {
+  const UploadTestPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('지역 데이터 업로드')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await uploadRegionSet();
+            // 사용자에게 업로드 완료 알림
+            if (context.mounted) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Firestore 업로드 완료!')),
+              );
+            }
+          },
+          child: const Text('지역 데이터 업로드하기'),
+        ),
+      ),
+    );
+  }
+}
