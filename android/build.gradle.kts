e31d9042ff34def897a21f699ead268fc30afd79
minamRyu <minamno5@naver.com>
2025-06-16 15:46:10 +0900

    20250616 카카오 로그인 성공

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,6 +12,9 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri("https://devrepo.kakao.com/nexus/content/groups/public/")
+        }
     }
 }
 
